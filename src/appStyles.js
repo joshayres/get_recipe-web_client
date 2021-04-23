b81c3d475c36fd10914b9e1febca10bf9c90c1c6
Josh Ayres <joshvayres10@gmail.com>
2021-04-23 14:01:15 -0400

    change color

--- src/appStyles.js
+++ src/appStyles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const AppWrap = styled.div`
   min-height: 100vh;
-  background: #ffb300;
+  background: #fbeee3;
 
   font-family: "Montserrat";
   color: #000000;
